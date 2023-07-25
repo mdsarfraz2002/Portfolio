bc22ebd44a3430933d8d216f6124a8415367f91c
Md Sarfraz Alam <84585300+mdsarfraz2002@users.noreply.github.com>
2023-07-25 05:02:04 +0000

    new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -65,17 +65,16 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card"> 
+        <Col md={4} className="project-card"> 
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Temperature & Humidity Forecast On Live Data"
+              description="Using arduino DHT-11 sensor to capture live temperature and humidity and using the live data to perform Data analysis using python"
+              ghLink="https://github.com/mdsarfraz2002/Live-temperature-forecast"
+              demoLink="https://github.com/mdsarfraz2002/Live-temperature-forecast/"  
             />
-          </Col>*/}
+          </Col>
         </Row>
       </Container>
     </Container>
